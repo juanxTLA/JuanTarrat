eda67160165dade0744f72a0f998c41fa3f0a50e
juanxTLA <jt0127@uah.edu>
2024-01-17 15:40:56 +0100

    Publish
    First version

---
+++ juantarrat/svelte.config.js
@@ -0,0 +1,18 @@
+import adapter from '@sveltejs/adapter-static';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+  kit: {
+    adapter: adapter({
+      // default options are shown
+      pages: 'build',
+      assets: 'build',
+      fallback: 'index.html'
+    }),
+    //target: '#svelte'
+  },
+  preprocess: vitePreprocess()
+};
+
+export default config;
